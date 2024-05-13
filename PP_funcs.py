66872b2591fc7e2fe289c68a66d74281a18f2811
mahdihbku <mla.mehdi@hotmail.com>
2024-05-13 13:54:30 +0300

    Clean code for non-PP FL

--- PP_funcs.py
+++ PP_funcs.py
@@ -9,24 +9,35 @@ import copy
 import numpy as np
 import torch
 
+def mask_model(seed, model, rand_high, rand_low):
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    for parm in model.parameters():
+        mask = torch.rand(parm.size()) * (rand_high - rand_low) + rand_low
+        parm.data += mask
+
+def unmask_model(seed, model, rand_high, rand_low):
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    for parm in model.parameters():
+        mask = torch.rand(parm.size()) * (rand_high - rand_low) + rand_low
+        parm.data -= mask
+
 def generate_masks_from_seed(seed, shapes, high, low):
     torch.manual_seed(seed)
     np.random.seed(seed)
-    masks = []
-    for shape in shapes:
-        mask = torch.rand(shape) * (high - low) + low
-        masks.append(mask)
+    masks = [torch.rand(shape)*(high-low)+low for shape in shapes]
     return masks
 
 def sum_list_masks(list_masks):
-    masks_sum = []
+    mask_size = len(list_masks[0])
     if len(list_masks) == 0:
         raise ValueError("The list of masks should not be empty.")
-    for j in range(len(list_masks[0])):
-        masks_sum.append(copy.deepcopy(list_masks[0][j]))
-        masks_sum[j].zero_()
+    masks_sum = [copy.deepcopy(list_masks[0][j]) for j in range(mask_size)]
     with torch.no_grad():
+        for j in range(mask_size):
+            masks_sum[j].zero_()
         for i in range(len(list_masks)):
-            for j in range(len(list_masks[0])):
+            for j in range(mask_size):
                 masks_sum[j] += list_masks[i][j]
     return masks_sum
--- main_with_masks copy.py
+++ main_with_masks copy.py
@@ -33,34 +33,14 @@ from models import *
 from tqdm import tqdm
 from FL_funcs import *
 from PP_funcs import *
-
-training_file = "data/train.p"
-validation_file = "data/valid.p"
-testing_file = "data/test.p"
-
-RAND_BIT_SIZE = 16
-rand_low = -100.0
-rand_high = +100.0
-
-# num_users = 23
-# num_participants = 5
-# num_clients= 20
-# num_rounds = 50
-# epochs = 20
-data_splits = 23
-num_participants = 5
-num_clients= 5
-num_rounds = 3
-epochs = 5
-LR=0.001
-MOMENTUM=0.9
-BATCH_SIZE = 32
-loss = 0
-
 torch.backends.cudnn.benchmark=True
 
 # to use GPU if possible
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    
+training_file = "data/train.p"
+validation_file = "data/valid.p"
+testing_file = "data/test.p"
 
 with open(training_file, mode='rb') as f:
     train = pickle.load(f)
@@ -76,34 +56,50 @@ X_test, y_test = test['features'], test['labels']
 print('Start...')
 
 torch.manual_seed(1)
+# num_users = 23
+# num_participants = 5
+# num_clients= 20
+# num_rounds = 50
+# epochs = 20
+
+num_users = 23
+num_participants = 5
+num_clients= 5
+num_rounds = 3
+epochs = 5
 
 train_dataset = ProcessDataset(training_file, transform=transforms.ToTensor())
 valid_dataset = ProcessDataset(validation_file, transform=transforms.ToTensor())
 test_dataset = ProcessDataset(testing_file, transform=transforms.ToTensor())
 
-traindata_split = torch.utils.data.random_split(train_dataset, [int(len(train_dataset) /  data_splits) for _ in range(data_splits)])
+traindata_split = torch.utils.data.random_split(train_dataset, [int(len(train_dataset) /  num_users) for _ in range(num_users)])
 
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-valid_loader = DataLoader(valid_dataset, batch_size=BATCH_SIZE, shuffle=False)
-test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+valid_loader = DataLoader(valid_dataset, batch_size=32, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
-train_loaders = [torch.utils.data.DataLoader(x, batch_size=BATCH_SIZE, shuffle=True) for x in traindata_split]
+train_loaders = [torch.utils.data.DataLoader(x, batch_size=32, shuffle=True) for x in traindata_split]
+
 
 train_loader = PrepareDataLoader(train_loader, to_device)
 valid_loader = PrepareDataLoader(valid_loader, to_device)
 test_loader = PrepareDataLoader(test_loader, to_device)
 
+
 train_loaders = [PrepareDataLoader(train_loaders[i], to_device) for i in range(len(train_loaders))]
 
 criterion = nn.CrossEntropyLoss()
+
+
 
 clients_models = [BasicNet().to(device) for _ in range(num_clients)] 
 
 criterion1 = nn.CrossEntropyLoss()
 criterion = nn.CrossEntropyLoss()
-opt = [optim.SGD(clients_models[i].parameters(), lr=LR, momentum=MOMENTUM) for i in range(num_clients)]
+opt = [optim.SGD(clients_models[i].parameters(), lr=0.001, momentum=0.9) for i in range(num_clients)]
 
 global_model = BasicNet().to(device)
+
 
 losses_training = []
 losses_testing = []
@@ -111,6 +107,10 @@ acc_training = []
 acc_testing = []
     
 participant_index = np.random.permutation(num_clients)[:num_participants]   
+loss = 0
+bit_size = 16
+low = -100.0
+high = +100.0
 
 for r in range(num_rounds):
 
@@ -121,7 +121,9 @@ for r in range(num_rounds):
         train_(epochs, clients_models[participant_index[i]], criterion1, opt[participant_index[i]], train_loaders[participant_index[i]], valid_loader)
 
     """Generating the seeds that will be used for masking"""
-    seeds = [random.getrandbits(RAND_BIT_SIZE) for i in range(num_participants)]
+    seeds = []
+    for i in range(num_participants):
+        seeds.append(random.getrandbits(bit_size))
 
     print("Aggregating without masking...")
     server_aggregate(global_model, clients_models)
@@ -129,23 +131,45 @@ for r in range(num_rounds):
 
     print("Masking clients_models...")
     for i in range(num_participants):
-        mask_model(seeds[i], clients_models[i], rand_high, rand_low)
+        torch.manual_seed(seeds[i])
+        np.random.seed(seeds[i])
+        for parm in clients_models[i].parameters():
+            # print("clients_models[0]: param name: {}".format(name))
+            # print("clients_models[0]: param.size(): {}".format(parm.size()))
+            # print("clients_models[0]: param param.requires_grad: {}".format(parm.requires_grad))
+            mask = torch.rand(parm.size()) * (high - low) + low # One mask for each parm
+            # print("clients_models[{}]: param name: {}".format(i,name))
+            # print("type(mask): {}".format(type(mask)))
+            # print("type(parm): {}".format(type(parm)))
+            # print("type(parm.data): {}".format(type(parm.data)))
+            parm.data += mask
     print("******* mask has been added")
+
     print("Central unmasking clients_models...")
     for i in range(num_participants):
-        unmask_model(seeds[i], clients_models[i], rand_high, rand_low)
+        torch.manual_seed(seeds[i])
+        np.random.seed(seeds[i])
+        for parm in clients_models[i].parameters():
+            mask = torch.rand(parm.size()) * (high - low) + low
+            parm.data -= mask
     print("******* mask has been removed")
     server_aggregate(global_model, clients_models)
     acc, loss = test_(global_model, criterion, test_loader)
 
+
     print("Central with masks list masking clients_models...")
     for i in range(num_participants):
-        mask_model(seeds[i], clients_models[i], rand_high, rand_low)
+        torch.manual_seed(seeds[i])
+        np.random.seed(seeds[i])
+        for parm in clients_models[i].parameters():
+            mask = torch.rand(parm.size()) * (high - low) + low # One mask for each parm
+            parm.data += mask
     print("******* mask has been added")
+
     print("Central with masks unmasking clients_models...")
     masks_shapes = [parm.size() for parm in clients_models[0].parameters()]
     for i in range(num_participants):
-        client_masks = generate_masks_from_seed(seeds[i], masks_shapes, rand_high, rand_low)
+        client_masks = generate_masks_from_seed(seeds[i], masks_shapes, high, low)
         for parm, mask in zip(clients_models[i].parameters(), client_masks):
             parm.data -= mask
     print("******* mask has been removed")
@@ -154,16 +178,35 @@ for r in range(num_rounds):
 
     print("Distributed masking clients_models...")
     for i in range(num_participants):
-        mask_model(seeds[i], clients_models[i], rand_high, rand_low)
+        torch.manual_seed(seeds[i])
+        np.random.seed(seeds[i])
+        for parm in clients_models[i].parameters():
+            mask = torch.rand(parm.size()) * (high - low) + low # One mask for each parm
+            parm.data += mask
     print("******* mask has been added")
+
     print("Distributed unmasking clients_models...")
     # Compute global mask
-    clients_masks = [generate_masks_from_seed(seeds[i], masks_shapes, rand_high, rand_low) for i in range(num_participants)]
+    clients_masks = []
+    for i in range(num_participants):
+        client_masks = generate_masks_from_seed(seeds[i], masks_shapes, high, low)
+        clients_masks.append(client_masks)
+
     global_mask = sum_list_masks(clients_masks)
+    
     server_aggregate_masked(global_model, clients_models, global_mask)
+
     print("******* mask has been removed")
     acc, loss = test_(global_model, criterion, test_loader)
 
+    # replicate_model(global_model, clients_models)
+
     print(f'##########{r+1}-th round')
+    # """Evalute The global model every global round"""
+    # acc, loss = test_(global_model, criterion, test_loader)
+    # losses_testing.append(loss)
+    # acc_testing.append(acc)
+    
+    # acc, loss = test_(global_model2, criterion, test_loader)
     # losses_testing.append(loss)
     # acc_testing.append(acc)
